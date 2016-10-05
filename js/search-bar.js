d07292ac645302e11b07f9bfb4222e34736f80e3
mcolic <milos.colic@amadeus.com>
2016-10-05 13:03:17 +0200

    search-page complete, seach-bar time-input fixed

--- js/search-bar.js
+++ js/search-bar.js
@@ -6,6 +6,16 @@ $(function () {
 });
 
 $(function () {
+    $('#sb-input-group-timepicker-start').datetimepicker({
+        format: 'LT',
+        stepping: 15,
+        debug: true
+    });
+    $('#sb-input-group-timepicker-end').datetimepicker({
+        format: 'LT',
+        stepping: 15,
+        debug: true
+    });
     $('#sb-input-group-timepicker').datetimepicker({
         format: 'LT',
         stepping: 15,
@@ -16,7 +26,8 @@ $(function () {
 $(function () {
     $('body').click(function() {
         $('#sb-input-group-datepicker').data("DateTimePicker").hide();
-        $('#sb-input-group-timepicker').data("DateTimePicker").hide();
+        $('#sb-input-group-timepicker-start').data("DateTimePicker").hide();
+        $('#sb-input-group-timepicker-end').data("DateTimePicker").hide();
         if($("#ignore_body_click").val()==="1" || $("#ignore_body_click").val()==="3") {
             $("#sb-dropdown-navigation").hide();
             $('#sb-menu-button').removeClass('sb-menu-button-active');
@@ -43,6 +54,8 @@ $(function () {
 
     $("#sb-input-group-datepicker").click(function(e){
         e.stopPropagation();
+        $('#sb-input-group-timepicker-start').data("DateTimePicker").hide();
+        $('#sb-input-group-timepicker-end').data("DateTimePicker").hide();
     });
 
     $('#sb-input-group-datepicker').find('.bootstrap-datetimepicker-widget').click(function(e){
@@ -54,16 +67,33 @@ $(function () {
         $('#sb-input-group-datepicker').data("DateTimePicker").toggle();
     });
 
-    $("#sb-input-group-timepicker").click(function(e){
+    $("#sb-input-group-timepicker-start").click(function(e){
         e.stopPropagation();
+        $("#sb-input-group-timepicker-end").data("DateTimePicker").hide();
+        $('#sb-input-group-datepicker').data("DateTimePicker").hide();
     });
 
-    $('#sb-input-group-timepicker').find('.bootstrap-datetimepicker-widget').click(function(e){
+    $('#sb-input-group-timepicker-start').find('.bootstrap-datetimepicker-widget').click(function(e){
         e.stopPropagation();
-        $('#sb-input-group-timepicker').data("DateTimePicker").show();
+        $('#sb-input-group-timepicker-start').data("DateTimePicker").show();
     });
 
-    $('#sb-input-group-timepicker').find('.sb-input').click(function(e){
-        $('#sb-input-group-timepicker').data("DateTimePicker").toggle();
+    $('#sb-input-group-timepicker-start').find('.sb-input').click(function(e){
+        $('#sb-input-group-timepicker-start').data("DateTimePicker").toggle();
+    });
+
+     $("#sb-input-group-timepicker-end").click(function(e){
+        e.stopPropagation();
+        $("#sb-input-group-timepicker-start").data("DateTimePicker").hide();
+        $('#sb-input-group-datepicker').data("DateTimePicker").hide();
+    });
+
+    $('#sb-input-group-timepicker-end').find('.bootstrap-datetimepicker-widget').click(function(e){
+        e.stopPropagation();
+        $('#sb-input-group-timepicker-end').data("DateTimePicker").show();
+    });
+
+    $('#sb-input-group-timepicker-end').find('.sb-input').click(function(e){
+        $('#sb-input-group-timepicker-end').data("DateTimePicker").toggle();
     });
 });
